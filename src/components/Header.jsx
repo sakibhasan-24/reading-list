8b093370baec3275e2cb75667099f348e22f69dc
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-31 01:48:21 +0600

    header part design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 export default function Header() {
   return (
-    <div>
-      <div>
+    <div className="mt-5 mx-2 bg-green-300 p-5 rounded-2xl">
+      <div className=" flex items-center content-center gap-7">
         <img
+          className="rounded-full w-[100px]"
           src="https://static.vecteezy.com/system/resources/previews/020/784/801/original/general-knowledge-icon-vector.jpg"
-          alt=""
+          alt="logo"
         />
+        <p className="text-4xl  font-extrabold tracking-wide text-blue-900">
+          add your topic
+        </p>
       </div>
     </div>
   );
